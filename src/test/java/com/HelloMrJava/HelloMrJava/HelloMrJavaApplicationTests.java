ef099d8751e051da7eda237f23498b59aa43ee39
diegolikescode <diegoprestes.git@hotmail.com>
2023-05-30 23:04:36 -0300

    init HelloMrJava

---
+++ src/test/java/com/HelloMrJava/HelloMrJava/HelloMrJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloMrJava.HelloMrJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMrJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
